591237e09bbe33370716f0cd8bcb03a551570fc0
Alexandr Zybailo <zero-pushkin@mail.ru>
2023-12-18 17:58:29 +0300

    Add Seminar3

--- Seminar1/Network/Program.cs
+++ Seminar1/Network/Program.cs
@@ -1,6 +1,9 @@
-﻿using System.Net.Sockets;
+﻿using System;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Network
 {
@@ -8,27 +11,43 @@ namespace Network
     {
         static void Main(string[] args)
         {
-            Server("Hello");
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Server("Hello", cts.Token);
+            Console.WriteLine("Нажмите любую клавишу для завершения работы сервера");
+            Console.ReadKey();
+            cts.Cancel();
         }
-        public static void Server(string name)
+        public static void Server(string name, CancellationToken token)
         {
             UdpClient udpClient = new UdpClient(12345);
             IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
-            Console.WriteLine("Ждем сообщение от клиента. Для завершения нажмите любую клавишу.");
-
-            while (!Console.KeyAvailable)
+        {
+            UdpClient udpClient = new UdpClient(12345);
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            Console.WriteLine("Ждем сообщение от клиента. Для завершения нажмите любую клавишу");
+            while (!token.IsCancellationRequested)
             {
-                byte[] buffer = udpClient.Receive(ref iPEndPoint);
+                 byte[] buffer;
+                try
+                {
+                    buffer = udpClient.Receive(ref iPEndPoint);
+                }
+                catch (SocketException ex) when (ex.SocketErrorCode == SocketError.Interrupted)
+                {
+                    Console.WriteLine("Работа сервера была прервана");
+                    return;
+                }
+
                 var messageText = Encoding.UTF8.GetString(buffer);
-                ThreadPool.QueueUserWorkItem(obj =>
+                Task.Run(() =>
                 {
                     Message message = Message.DesirializeFromJson(messageText);
                     message.Print();
 
                     byte[] reply = Encoding.UTF8.GetBytes("Сообщение получено");
                     udpClient.Send(reply, reply.Length, iPEndPoint);
-                });
+                    }, token);
             }
-         }
+        }
     }
 }
